25f2475b7bad1dba2f2bef32265a9fe92d3efac6
muhammad saad <ms22458881@gmail.com>
2025-09-11 23:13:49 +0500

    update

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,30 +1,37 @@
 import React, { useEffect } from 'react'
-import img1 from '../assets/quick_calligraphy.jpg'
+import img1 from '../assets/zyrawear.jpg'
 import img2 from '../assets/electro.jpg'
-import img3 from '../assets/zyrawear.jpg'
+import img3 from '../assets/ecommerce_brand.jpg'
+import img4 from '../assets/business_nexus.jpg'
 
 
 function Project() {
   
-  const projects=[
+  const projects=[    
     {
       img:img1,
-      title:"quick calligraphy",
-      des:"My Fronted project where different categories calligraphy designs are available for kids and adult category and information given as well. ",
-      link:"http://std1431528.aptech-students.com/"
+      title:"ZyraWear",
+      des:"Zyrawear is my first eCommerce web application built using React. It features a modern UI, dynamic product listings, cart functionality, and responsive design ",
+      link:"https://zyrawear-project.vercel.app/",
     },
     {
       img:img2,
-      title:"Electro",
+      title:"Electro – eCommerce Website",
       des:"My Laravel E-commerce project on 'Electro' where the user order electronic products and also admin panel integrated where products, categories user details order details are saved also total products categories are define on dashboard",
       link:"https://www.linkedin.com/posts/muhammad-saad-ab7879279_laravel-reactjs-php-activity-7258818334094204928-X5ev?utm_source=share&utm_medium=member_desktop"
     },
-     {
-       img:img3,
-       title:"ZyraWear",
-       des:"My first react E-commerce project where I create my own api and fetch products in categories wise also login/signup page with local storage ",
-       link:"https://zyrawear-project.vercel.app/",
-     }
+    {
+      img:img3,
+      title:"Brand – eCommerce Website",
+      des:"Brand is a modern eCommerce platform developed using Laravel (backend), React.js (frontend), and Inertia.js for seamless single-page application behavior.",
+      link:"https://www.linkedin.com/posts/muhammad-saad-ab7879279_e-commerce-web-application-built-with-activity-7360982592021553152-Vn4r?utm_source=share&utm_medium=member_desktop&rcm=ACoAAEPwKesBrEHebWerHM1jcVjG5iKOcp9mPgk"
+    },
+    {
+      img:img4,
+      title:"Business Nexus portal ",
+      des:"Business Nexus is an interactive platform designed to bridge the gap between investors and entrepreneurs. It provides a collaborative space where startups can showcase their ideas and connect with potential investors, while investors can discover promising ventures—all within a streamlined, user-friendly panel.",
+      link:"https://www.linkedin.com/posts/muhammad-saad-ab7879279_business-nexus-collaboration-platform-activity-7370884634924015616-00LI?utm_source=share&utm_medium=member_desktop&rcm=ACoAAEPwKesBrEHebWerHM1jcVjG5iKOcp9mPgk"
+    },
     
   ]
     const data=()=>{
